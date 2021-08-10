0c422501561ebff183a87a70957a9784015c0bb7
Gerardo Abraham <geoabraham@tuta.io>
2021-08-10 13:14:44 -0300

    Add solution mortgage.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,17 +4,22 @@
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
+months = 0
 extra_payment = 1000.0
-months_extra_payment = 12
+extra_payment_start_month = 61
+extra_payment_end_month = 108
 total_paid = 0.0
 
 while principal > 0:
-    if months_extra_payment != 0:
-        principal = principal * (1 + rate / 12) - payment - extra_payment
-        total_paid = total_paid + payment + extra_payment
-        months_extra_payment -= 1
-    else:
-        principal = principal * (1 + rate / 12) - payment
-        total_paid = total_paid + payment
+    months += 1
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
 
-print('Total paid', total_paid)
+    if extra_payment_start_month <= months >= extra_payment_end_month:
+        principal = principal - extra_payment
+        total_paid = total_paid + extra_payment
+
+    print(f'{months} {round(total_paid, 2)} {round(principal, 2)}')
+
+print(f'Total paid {total_paid}')
+print(f'Months {months}')
