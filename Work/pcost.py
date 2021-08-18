f146203b11344c56383722502ab7abc7bfe9a886
Gerardo Abraham <geoabraham@tuta.io>
2021-08-18 11:56:37 -0300

    Resolve 1.31

--- Work/pcost.py
+++ Work/pcost.py
@@ -6,14 +6,23 @@ def portfolio_cost(filename):
 
     with open(filename, 'rt') as f:
         header = next(f)
+        num_shares = 0.0
+        price = 0.0
+
         for line in f:
             row = line.split(',')
-            num_shares = int(row[1])
-            price = float(row[2])
+
+            try:
+                num_shares = int(row[1])
+                price = float(row[2])
+            except ValueError:
+                print("Couldn't parse", line)
+
             total += num_shares * price
 
     return total
 
 
+cost = portfolio_cost('Work/Data/missing.csv')
 cost = portfolio_cost('Work/Data/portfolio.csv')
 print(f'Total cost: {cost}')
