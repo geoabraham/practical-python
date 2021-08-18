6245d465603cc18ec061b66495aa16e9dbe1b6fc
Gerardo Abraham <geoabraham@tuta.io>
2021-08-18 11:37:50 -0300

    Resolver exercise 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,15 +1,19 @@
 # pcost.py
 #
 # Exercise 1.27
+def portfolio_cost(filename):
+    total = 0.0
 
-total = 0.0
+    with open(filename, 'rt') as f:
+        header = next(f)
+        for line in f:
+            row = line.split(',')
+            num_shares = int(row[1])
+            price = float(row[2])
+            total += num_shares * price
 
-with open('Work/Data/portfolio.csv', 'rt') as f:
-    header = next(f)
-    for line in f:
-        row = line.split(',')
-        num_shares = int(row[1])
-        price = float(row[2])
-        total += num_shares * price
+    return total
 
-print(f'Total cost: {total}')
+
+cost = portfolio_cost('Work/Data/portfolio.csv')
+print(f'Total cost: {cost}')
