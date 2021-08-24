22bfbd2e659d8620c81fc24ecd2815c25f357f4a
Gerardo Abraham <geoabraham@tuta.io>
2021-08-24 12:33:06 -0300

    Resolve 2.12

--- Work/report.py
+++ Work/report.py
@@ -1,6 +1,6 @@
 # report.py
 #
-# Exercise 2.11
+# Exercise 2.12
 import csv
 
 
@@ -52,7 +52,36 @@ def read_prices(filename):
     return stock_prices
 
 
-def make_report(portfolio, prices):
+def get_report_data(portfolio, prices):
+    """Get a list of tuples given a portfolio and stock prices
+
+    Args:
+        portfolio (list): a portfolio
+        prices (dict): stock prices
+
+    Returns:
+        [list]: list of tuples (name, shares, price, change)
+    """
+    report_data = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        report_data.append(
+            (stock['name'], stock['shares'], current_price, change))
+
+    return report_data
+
+
+def make_total_report(portfolio, prices):
+    """Make Report
+
+    Args:
+        portfolio (list): portfolio list
+        prices (dict): stock prices
+
+    Returns:
+        list: report
+    """
     total_cost = 0.0
 
     for stock in portfolio:
@@ -67,11 +96,31 @@ def make_report(portfolio, prices):
     print(f'Total value: {total_value}')
     print(f'Gain: {(total_value - total_cost):0.2f}')
 
-    return total_value
+
+def make_report(data):
+    """Make and print on screen stocks report
+
+    Args:
+        data (list): data
+    """
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(
+        f'{headers[0]:>10s} {headers[1]:>10s} {headers[2]:>10s} {headers[3]:>10s}')
+    print(('-' * 10 + ' ') * len(headers))
+    for row in data:
+        formated_price = f'${row[2]:.2f}'
+        print(
+            f'{row[0]:>10s} {row[1]:>10d} {formated_price:>10s} {row[3]:>10.2f}')
 
 
+# Read files
 portfolio = read_portfolio('Work/Data/portfolio.csv')
 prices = read_prices('Work/Data/prices.csv')
-data = make_report(portfolio, prices)
 
-print(f'Data: {data}')
+# Get data
+data = get_report_data(portfolio, prices)
+
+# Make report
+make_report(data)
+
+report = make_total_report(portfolio, prices)
